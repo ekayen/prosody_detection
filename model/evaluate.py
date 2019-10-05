36fb20cd5ec0a53f28bcc73cb816c47389517056
ekayen <ekayen@gmail.com>
2019-10-05 15:25:47 +0100

    put eval fn in own file

--- model/evaluate.py
+++ model/evaluate.py
@@ -1,32 +1,54 @@
 from utils import load_data
 import numpy as np
+import pickle
+import torch
+from seqeval.metrics import accuracy_score, classification_report,f1_score
 
-def dummy_predict(seq):
-    seq = seq.split()
-    return np.random.randint(2,size=len(seq))
 
-def evaluate(instance):
-    seq, lbl = instance
-    print(seq,lbl)
-    prediction = dummy_predict(seq) # TODO eventually this'll probably need to be a np int represenatation of the seq
-    print(prediction)
+def evaluate(X, Y,mdl,device,to_file=False):
+    print("EVAL=================================================================================================")
+    y_pred = []
+    with torch.no_grad():
+        for i in range(len(X)):
+            #input = torch.tensor([i if i in i_to_wd else wd_to_i['<UNK>'] for i in input])
+            input = X[i].to(device)
+            eval_batch_size = 1
+
+            if not (list(input.shape)[0] == 0):
+                hidden = mdl.init_hidden(eval_batch_size)
+                tag_scores, _ = mdl(input.view(len(input),eval_batch_size), hidden)
+
+                #pred = np.squeeze(np.argmax(tag_scores, axis=-1)).tolist() # TODO could this be wrong? Almost certainly yes.
+
+                pred = tag_scores.cpu()
+                pred = np.where(pred>0.5,1,0)
+                pred = np.squeeze(pred)
+                pred = pred.tolist()
+                if type(pred) is int:
+                    pred = [pred]
+                #pred = [[str(j) for j in i] for i in pred]
+                pred = [str(j) for j in pred]
+
+                #y_pred += pred
+                y_pred.append(pred)
+    print('Evaluation:')
     #import pdb;pdb.set_trace()
-    tp = np.sum(np.logical_and(prediction == 1, lbl == 1))
-    fp = np.sum(np.logical_and(prediction == 1, lbl == 0))
-    tn = np.sum(np.logical_and(prediction == 0, lbl == 0))
-    fn = np.sum(np.logical_and(prediction == 0, lbl == 1))
+    #Y = np.concatenate([y.cpu().flatten() for y in Y]).tolist()
+    #y_pred = np.concatenate([y.flatten() for y in y_pred]).tolist()
+    #Y = [str(y) for y in Y]
+    #y_pred = [str(y) for y in y_pred]
+    if to_file:
+        with open('Y_true.pkl','wb') as f:
+            pickle.dump(Y,f)
+        with open('Y_pred.pkl','wb') as f:
+            pickle.dump(y_pred,f)
+        with open('X.pkl','wb') as f:
+            pickle.dump(X,f)
 
-    print(tp,tn,fp,fn)
-
-def evaluate_all(filename):
-    data = load_data(filename)
-    evaluate(data[0])
-
-
-
-
-def main():
-    evaluate_all('data/all_acc.pickle')
-
-if __name__=='__main__':
-    main()
+    f1 = f1_score(Y, y_pred)
+    acc = accuracy_score(Y, y_pred)
+    clss = classification_report(Y, y_pred)
+    print('F1:',f1)
+    print('Acc',acc)
+    print(clss)
+    return(f1,acc,clss)
--- model/model.py
+++ model/model.py
@@ -11,8 +11,8 @@ torch.manual_seed(0)
 import torch.nn.functional as F
 
 from utils import load_data,BatchWrapper,to_ints,load_vectors,make_batches,load_libri_data
+from evaluate import evaluate
 import numpy as np
-from seqeval.metrics import accuracy_score, classification_report,f1_score
 
 PRINT_DIMS = False
 PRINT_EVERY = 50
@@ -215,53 +215,6 @@ model.to(device)
 
 # DEFINE EVAL FUNCTION
 
-def evaluate(X, Y,mdl,to_file=False):
-    print("EVAL=================================================================================================")
-    y_pred = []
-    with torch.no_grad():
-        for i in range(len(X)):
-            #input = torch.tensor([i if i in i_to_wd else wd_to_i['<UNK>'] for i in input])
-            input = X[i].to(device)
-            eval_batch_size = 1
-
-            if not (list(input.shape)[0] == 0):
-                hidden = mdl.init_hidden(eval_batch_size)
-                tag_scores, _ = mdl(input.view(len(input),eval_batch_size), hidden)
-
-                #pred = np.squeeze(np.argmax(tag_scores, axis=-1)).tolist() # TODO could this be wrong? Almost certainly yes.
-
-                pred = tag_scores.cpu()
-                pred = np.where(pred>0.5,1,0)
-                pred = np.squeeze(pred)
-                pred = pred.tolist()
-                if type(pred) is int:
-                    pred = [pred]
-                #pred = [[str(j) for j in i] for i in pred]
-                pred = [str(j) for j in pred]
-
-                #y_pred += pred
-                y_pred.append(pred)
-    print('Evaluation:')
-    #import pdb;pdb.set_trace()
-    #Y = np.concatenate([y.cpu().flatten() for y in Y]).tolist()
-    #y_pred = np.concatenate([y.flatten() for y in y_pred]).tolist()
-    #Y = [str(y) for y in Y]
-    #y_pred = [str(y) for y in y_pred]
-    if to_file:
-        with open('Y_true.pkl','wb') as f:
-            pickle.dump(Y,f)
-        with open('Y_pred.pkl','wb') as f:
-            pickle.dump(y_pred,f)
-        with open('X.pkl','wb') as f:
-            pickle.dump(X,f)
-
-    f1 = f1_score(Y, y_pred)
-    acc = accuracy_score(Y, y_pred)
-    clss = classification_report(Y, y_pred)
-    print('F1:',f1)
-    print('Acc',acc)
-    print(clss)
-    return(f1,acc,clss)
 
 Y_train_str = [[str(i) for i in y.tolist()] for y in Y_train]
 #Y_train_batches_str = [[[str(i) for i in inst] for inst in batch.numpy()] for batch in Y_train_batches]
@@ -349,11 +302,11 @@ for epoch in range(NUM_EPOCHS):
                 train_loss = (sum(recent_losses)/len(recent_losses)).item()
                 train_losses.append(train_loss)
                 if DATASOURCE == 'SWBDNXT':
-                    _,train_acc,_ = evaluate(X_train, Y_train_str, model)
+                    _,train_acc,_ = evaluate(X_train, Y_train_str, model,device)
                     train_accs.append(train_acc)
                 else:
                     train_accs.append(0)
-                _, dev_acc, _ = evaluate(X_dev, Y_dev_str, model)
+                _, dev_acc, _ = evaluate(X_dev, Y_dev_str, model,device)
                 dev_accs.append(dev_acc)
                 timesteps.append(timestep)
 
