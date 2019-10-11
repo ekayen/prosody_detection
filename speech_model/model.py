f9d835fff885c3718c5b04f514464d053049bda6
ekayen <ekayen@gmail.com>
2019-10-11 18:55:21 +0100

    added more to model, incl forward -- still not functional

--- speech_model/model.py
+++ speech_model/model.py
@@ -23,15 +23,6 @@ df = pd.read_csv(text_data,sep='\t')
 utt_keys = set(df.iloc[:,1].tolist())
 
 
-t1 = time.time()
-print("Loading in dict ...")
-with open(speech_data,'rb') as f:
-    utt_dict = pickle.load(f)
-t2 = time.time()
-print("Time to load: ",t2-t1)
-
-process = psutil.Process(os.getpid())
-print('Memory usage:',process.memory_info().rss/1000000000, 'GB')
 
 class MaskConv(nn.Module):
     """
@@ -93,14 +84,11 @@ class SequenceWise(nn.Module):
 
 class SpeechEncoder(nn.Module):
     def __init__(self,
-                 rnn_type=nn.LSTM,
                  rnn_hidden_size=512,
                  bidirectional=True,
                  num_rnn_layers=3,
-                 seq_len=800,
                  num_classes=2):
         super(SpeechEncoder,self).__init__()
-        self.rnn_type = rnn_type
         self.rnn_hidden_size = rnn_hidden_size
         self.bidirectional = bidirectional
         self.num_rnn_layers = num_rnn_layers
@@ -115,7 +103,6 @@ class SpeechEncoder(nn.Module):
         self.stride1 = (2,self.feat_dim)
         self.stride2 = (2,1)
         self.padding = (4,0)
-        self.seq_len = seq_len
 
         self.conv = MaskConv(nn.Sequential(
             nn.Conv2d(self.in_channels, self.hidden_channels, kernel_size=self.kernel1, stride=self.stride1,
@@ -128,8 +115,8 @@ class SpeechEncoder(nn.Module):
             nn.ReLU(inplace=True)
         ))
 
-        rnn_input_size = self.seq_len
-        rnn_input_size = (rnn_input_size- self.kernel1[0] + 2 * self.padding[0]) / (1 + self.stride1[0])
+        rnn_input_size = self.seq_len # TODO FIRST figure out what the seq_len is, in fact
+        rnn_input_size = (rnn_input_size - self.kernel1[0] + 2 * self.padding[0]) / (1 + self.stride1[0])
         rnn_input_size = (rnn_input_size - self.kernel2[0] + 2 * self.padding[0]) / (1 + self.stride2[0])
         self.lstm = nn.LSTM(input_size=rnn_input_size,
                             hidden_size=self.rnn_hidden_size,
@@ -145,3 +132,55 @@ class SpeechEncoder(nn.Module):
         )
         self.inference_softmax = nn.Softmax()
 
+
+    def forward(self, x, lengths):
+        lengths = lengths.int() # TODO figure out if .cpu() is needed here
+        output_lengths = self.get_seq_lens(lengths)
+        x, _ = self.conv(x, output_lengths)
+
+        sizes = x.size()
+        x = x.view(sizes[0], sizes[1] * sizes[2], sizes[3])  # Collapse feature dimension
+        x = x.transpose(1, 2).transpose(0, 1).contiguous()  # TxNxH
+
+        for rnn in self.rnns:
+            x = rnn(x, output_lengths)
+
+        if not self.bidirectional:  # no need for lookahead layer in bidirectional
+            x = self.lookahead(x)
+
+        x = self.fc(x)
+        x = x.transpose(0, 1)
+        # identity in training mode, softmax in eval mode
+        x = self.inference_softmax(x)
+        return x, output_lengths
+
+    def get_seq_lens(self, input_length):
+        """
+        Given a 1D Tensor or Variable containing integer sequence lengths, return a 1D tensor or variable
+        containing the size sequences that will be output by the network.
+        :param input_length: 1D Tensor
+        :return: 1D Tensor scaled by model
+        """
+        seq_len = input_length
+        for m in self.conv.modules():
+            if type(m) == nn.modules.conv.Conv2d:
+                seq_len = ((seq_len + 2 * m.padding[1] - m.dilation[1] * (m.kernel_size[1] - 1) - 1) / m.stride[1] + 1)
+        return seq_len.int()
+
+
+t1 = time.time()
+print("Loading in dict ...")
+with open(speech_data,'rb') as f:
+    utt_dict = pickle.load(f)
+t2 = time.time()
+print("Time to load: ",t2-t1)
+
+model = SpeechEncoder(rnn_hidden_size=512,bidirectional=True,num_rnn_layers=3,num_classes=2)
+
+instance = utt_dict[utt_keys[57]]
+
+model(instance,instance.shape[0])
+
+
+process = psutil.Process(os.getpid())
+print('Memory usage:',process.memory_info().rss/1000000000, 'GB')
