022e7689639533ddb21accdeee4b30716431b6da
ekayen <ekayen@gmail.com>
2019-11-11 15:19:31 +0000

    cross validation scripts

---
+++ text_model/run_crossval.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+nice python model.py conf/crossval/seed43.yaml
+nice python model.py conf/crossval/seed44.yaml
+nice python model.py conf/crossval/seed45.yaml
+nice python model.py conf/crossval/seed46.yaml
+nice python model.py conf/crossval/seed47.yaml
+nice python model.py conf/crossval/seed48.yaml
+nice python model.py conf/crossval/seed49.yaml
+nice python model.py conf/crossval/seed50.yaml
+nice python model.py conf/crossval/seed51.yaml
