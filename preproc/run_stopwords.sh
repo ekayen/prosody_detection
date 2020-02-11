5637a778a40cde419382c4de4eb8f35521a17ecd
Elizabeth Nielsen <ekayen@gmail.com>
2020-02-11 18:13:25 +0000

    stopwod stuff

---
+++ preproc/run_stopwords.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+python make_stopword_list.py tenfold0
+python make_stopword_list.py tenfold1
+python make_stopword_list.py tenfold2
+python make_stopword_list.py tenfold3
+python make_stopword_list.py tenfold4
+python make_stopword_list.py tenfold5
+python make_stopword_list.py tenfold6
+python make_stopword_list.py tenfold7
+python make_stopword_list.py tenfold8
+python make_stopword_list.py tenfold9
+python make_stopword_list.py fivefold0
+python make_stopword_list.py fivefold1
+python make_stopword_list.py fivefold2
+python make_stopword_list.py fivefold3
+python make_stopword_list.py fivefold4
+python make_stopword_list.py heldout_f1a
+python make_stopword_list.py heldout_f2b
+python make_stopword_list.py heldout_f3a
+python make_stopword_list.py heldout_m1b
+python make_stopword_list.py heldout_m2b
+python make_stopword_list.py f2b_only0
+python make_stopword_list.py f2b_only1
+python make_stopword_list.py f2b_only2
+python make_stopword_list.py f2b_only3
+python make_stopword_list.py f2b_only4
+python make_stopword_list.py f2b_only5
+python make_stopword_list.py f2b_only6
+python make_stopword_list.py f2b_only7
+python make_stopword_list.py f2b_only8
+python make_stopword_list.py f2b_only9
+
