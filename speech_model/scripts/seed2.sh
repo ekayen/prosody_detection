a589c4dc6c3ad218a5cd30c3fcfbfe5aa66560d7
ekayen <ekayen@gmail.com>
2020-02-22 17:18:14 +0000

    add model for rate prediction

---
+++ speech_model/scripts/seed2.sh
@@ -0,0 +1,2 @@
+python3 train.py -c conf/cnn_lstm_best.yaml  -d ../data/burnc/splits/tenfold4.yaml -v 3000 -s $1 &
+python3 train.py -c conf/cnn_lstm_best.yaml  -d ../data/burnc/splits/tenfold5.yaml -v 3000 -s $1 &
